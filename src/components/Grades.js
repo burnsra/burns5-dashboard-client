e300a3d9e1bc3271d74e75342c51a8f6042e72f0
Robert Burns <burnsra@gmail.com>
2017-10-15 10:26:41 -0400

    Addition of Grades component

---
+++ src/components/Grades.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import DataService from '../services/api/data'
+
+class Grades extends Component {
+  state = {
+    grades: [],
+    updated: ""
+  };
+
+  componentDidMount() {
+    DataService
+      .get('grades')
+      .then((response) => {
+        this.setState({
+          grades: response.detail,
+          updated: response.updated
+        });
+      });
+  }
+
+  render() {
+    const { grades } = this.state;
+    const { updated } = this.state;
+    var arrGrades = [];
+
+    Object.keys(grades).forEach(function(name) {
+      var student = {};
+      var studentCourses = [];
+
+      student.name = name;
+      Object.keys(grades[name]).forEach(function(course) {
+        studentCourses.push(grades[name][course])
+      });
+      student.courses = studentCourses;
+      arrGrades.push(student);
+    });
+
+    return (
+      [
+      <div className="Grades">
+      <table className="table table-bordered table-striped table-sm">
+          <thead className="thead-inverse"><tr><th width="66%">Course</th><th>Grade</th></tr></thead>
+            {arrGrades.map( (item,index) =>
+              <StudentDetail key={index} index={index} name={item.name} courses={item.courses} />
+            )}
+          <tfoot><tr className="table-active"><td colSpan="2"><small>Last Updated { updated }</small></td></tr></tfoot>
+      </table>
+      </div>
+    ]
+    );
+  }
+}
+
+class StudentDetail extends React.Component {
+  render() {
+    return (
+      [
+        <tbody><DataTitle key={this.props.index} index={this.props.index} name={this.props.name} /></tbody>,
+        <tbody>{this.props.courses.map( (item,index) =>
+          <MyGradesChild key={index} index={index} course={item.course} grade={item.grade} />
+        )}</tbody>
+      ]
+    );
+  }
+}
+
+class DataTitle extends React.Component {
+  render() {
+    return (
+        <tr><th className="table-active" colSpan="2"><strong>{this.props.name}</strong></th></tr>
+    );
+  }
+}
+
+class MyGradesChild extends React.Component {
+  render() {
+    return <tr><td width="66%">{this.props.course}</td><td>{this.props.grade}</td></tr>
+  }
+}
+
+export default Grades;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
+import Grades from './Grades'
 import Schedule from './Schedule'
 import Wireless from './Wireless'
 
@@ -10,7 +11,7 @@ class Router extends Component {
       <div className="container">
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route path='/grades' component={Home}/>
+          <Route path='/grades' component={Grades}/>
           <Route path='/schedules' component={Schedule}/>
           <Route path='/wireless' component={Wireless}/>
         </Switch>
