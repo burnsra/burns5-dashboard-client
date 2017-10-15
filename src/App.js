4e9a596863102a68126a21c62eb62552d09f792d
Robert Burns <burnsra@gmail.com>
2017-10-15 08:16:49 -0400

    Begin to personalize content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <h1>Burns5 Dashboard</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          Dashboard for the Burns5 family.
         </p>
       </div>
     );
