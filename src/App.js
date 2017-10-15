453f3ad1697f1e96a9902ced28fa393fa63e80ee
Robert Burns <burnsra@gmail.com>
2017-10-15 08:44:37 -0400

    Addition of Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>Burns5 Dashboard</h1>
-        </header>
+        <Header />
         <p>
           Dashboard for the Burns5 family.
         </p>
---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav } from 'reactstrap';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  _toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  _closeNav() {
+    document.querySelector('.navbar-collapse').classList.remove('show');
+    this.setState({
+      collapsed: true
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <Navbar color="dark" className="navbar-dark navbar-expand-sm" expand="false" fixed="top">
+          <NavbarBrand href="/">Burns5 Dashboard</NavbarBrand>
+          <NavbarToggler onClick={this._toggleNavbar.bind(this)} />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav className="mr-auto" navbar>
+              <li className="nav-item">
+                <NavLink exact className="nav-link" to='/' onClick={this._closeNav.bind(this)}>Home</NavLink>
+              </li>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </header>
+    );
+  }
+}
+
+export default Header;
